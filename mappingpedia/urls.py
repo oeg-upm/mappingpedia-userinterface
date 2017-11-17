47c6c8d581887e8933ba39bd599f6b35babc3ce1
Ahmad Alobaid <ahmad88me@gmail.com>
2017-11-17 17:05:16 +0100

    initial

---
+++ mappingpedia/urls.py
@@ -0,0 +1,28 @@
+"""mappingpedia URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+import views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url('dataset_view', views.dataset_view),
+    url('dataset_register', views.dataset_register),
+    url('mapping_view', views.mapping_view),
+    url('mapping_register', views.mapping_register),
+    url('execution_view', views.execute_view),
+    url('execute', views.execute_mapping),
+]
---
+++ mappingpedia/views.py
@@ -0,0 +1,81 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+import os
+import requests
+import json
+
+mappingpedia_engine_base_url = "http://mappingpedia-engine.linkeddata.es"
+# mappingpedia_engine_base_url = "http://localhost:8090"
+#mappingpedia_engine_base_url = "http://127.0.0.1:80/"
+organization_id = "zaragoza_test"
+
+
+def dataset_view(request):
+    return render(request, 'dataset_view.html')
+
+
+def dataset_register(request):
+    distribution_download_url = request.POST['url']
+    url = os.path.join(mappingpedia_engine_base_url, 'datasets', organization_id)
+    data = {
+        "distribution_download_url": distribution_download_url,
+    }
+    response = requests.post(url, data)
+    if response.status_code == 200:
+        dataset_id = json.loads(response.content)['dataset_id']
+        return render(request, 'msg.html', {'msg': 'The dataset has been registered with id = '+dataset_id})
+    else:
+        return render(request, 'msg.html', {'msg': response.content})
+
+
+def mapping_register(request):
+    print request.POST
+    dataset_id = request.POST['dataset_id']
+    mapping_file_url = request.POST['mapping_file_url']
+    url = os.path.join(mappingpedia_engine_base_url, 'mappings', organization_id, dataset_id)
+    data = {
+        "mappingDocumentDownloadURL": mapping_file_url
+    }
+    response = requests.post(url, data)
+    if response.status_code == 200:
+        print response.content
+        download_url = json.loads(response.content)['mapping_document_download_url']
+        return render(request, 'msg.html', {'msg': 'mappings has been registered with download_url: '+download_url})
+    else:
+        return render(request, 'msg.html', {'msg': 'error: '+response.content})
+
+
+def mapping_view(request):
+    return render(request, 'mapping_view.html')
+
+
+# def get_mappings_for_dataset(request):
+#     dataset_id = request.POST['dataset_id']
+#     url = os.path.join(mappingpedia_engine_base_url, 'mappings', 'findMappingDocumentsByDatasetId?='+)
+#     url = os.path.join(mappingpedia_engine_base_url)
+#     requests.post()
+
+
+def execute_view(request):
+    return render(request, 'execute_view.html')
+
+
+def execute_mapping(request):
+    mapping_filename = request.POST['mapping_filename']
+    dataset_id = request.POST['dataset_id']
+    distribution_download_url = request.POST['distribution_url']
+    url = os.path.join(mappingpedia_engine_base_url, 'executions', organization_id, dataset_id, mapping_filename)
+    print url
+    data = {
+        "distribution_download_url": distribution_download_url,
+    }
+    print "data: "
+    print data
+    response = requests.post(url, data)
+    if response.status_code is 200:
+        result_url = json.loads(response.content)['mapping_execution_result_download_url']
+        print "result : "
+        print response.content
+        return render(request, 'msg.html', {'msg': 'Execution results can be found here %s'%result_url})
+    else:
+        return render(request, 'msg.html', {'msg': 'error: '+response.content})
