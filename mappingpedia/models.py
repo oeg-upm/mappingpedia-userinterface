30cd5b7ca33070741780a3a0e13ac7c4b60993b5
Ahmad Alobaid <ahmad88me@gmail.com>
2018-02-07 16:20:39 +0100

    add Execution Progress

---
+++ mappingpedia/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from datetime import datetime
+
+
+class ExecutionProgress(models.Model):
+    STATUS_CHOICES = (
+        ('inprogress', 'In Progress'),
+        ('success', 'Success'),
+        ('fail', 'Fail'),
+    )
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES)
+    user = models.CharField(max_length=250)
+    timestamp = models.DateTimeField(default=datetime.now())
+    result_page = models.URLField(max_length=250)
+
+
